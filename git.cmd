3036e0b1e99b07cb239629a6280f23ba1b53ac32
houfukude <huofukude@huofukude-Y450.(none)>
2012-07-18 13:29:42 +0800

    update git.cmd

--- git.cmd
+++ git.cmd
@@ -1,1 +1,6 @@
-git push https://kuagedmacosx@code.google.com/p/scnuthesis/ master //push 项目
+cd /home/huofukude/userdata/python/django/vod-music
+git push https://kuagedmacosx@code.google.com/p/scnuthesis/ master //第一次push 项目
+git remote -v //再次显示远程仓库
+git add 
+git commit
+git push //push项目
